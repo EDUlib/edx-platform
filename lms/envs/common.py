5a5affa9cb49e6e3bc10462eb14b8f2f50a125b4
Oper Edulib <oper.edulib@gmail.com>
2015-03-19 16:16:20 -0400

    Modified common.py in LMS for COURSE_CATALOG_VISIBILITY_PERMISSION="see_in_catalog" COURSE_ABOUT_VISIBILITY_PERMISSION="see_about_page"

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2182,6 +2182,7 @@ INSTALLED_APPS = (
 
     'sorl.thumbnail',
 
+##### EUCALYPTUS CODE
     # Credentials support
     'openedx.core.djangoapps.credentials',
 
@@ -2830,11 +2831,13 @@ COUNTRIES_OVERRIDE = {
 
 # which access.py permission name to check in order to determine if a course is visible in
 # the course catalog. We default this to the legacy permission 'see_exists'.
-COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_exists'
+#COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_exists'
+COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_in_catalog'
 
 # which access.py permission name to check in order to determine if a course about page is
 # visible. We default this to the legacy permission 'see_exists'.
-COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_exists'
+#COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_exists'
+COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_about_page'
 
 
 # Enrollment API Cache Timeout
