6a1df1fda50ca69b77e0d523ee89a4f293c1d39b
Oper Edulib <oper.edulib@gmail.com>
2016-02-04 14:53:08 -0500

    Small modifation for theme social media being used Removing a file no longer necessary Adding what_is_verified_cert to static_templates

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2399,10 +2399,7 @@ SOCIAL_SHARING_SETTINGS = {
 SOCIAL_MEDIA_FOOTER_NAMES = [
     "facebook",
     "twitter",
-    "youtube",
-    "linkedin",
-    "google_plus",
-    "reddit",
+    "google_plus"
 ]
 
 # JWT Settings
