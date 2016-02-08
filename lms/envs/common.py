62b51059d93161175f7b3b8b3908f6af3d74f7c4
Oper Edulib <oper.edulib@gmail.com>
2016-02-08 14:08:07 -0500

    Changing the values in common.py that lets us use {cert_name_short} and {cert_name_long} Modifying the values in the translation files to reuse them now

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2507,8 +2507,10 @@ REGISTRATION_FIELD_ORDER = [
 REGISTRATION_EMAIL_PATTERNS_ALLOWED = None
 
 ########################## CERTIFICATE NAME ########################
-CERT_NAME_SHORT = "Certificate"
-CERT_NAME_LONG = "Certificate of Achievement"
+#CERT_NAME_SHORT = "Certificate"
+CERT_NAME_SHORT = "attestation"
+#CERT_NAME_LONG = "Certificate of Achievement"
+CERT_NAME_LONG = "attestation de réussite"
 
 #################### OpenBadges Settings #######################
 
