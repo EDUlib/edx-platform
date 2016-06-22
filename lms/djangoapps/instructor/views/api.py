f2fcea3b495f2f3cce17622cfd2325eda441e969
Oper Edulib <oper.edulib@gmail.com>
2016-06-22 18:08:59 -0400

    Reapplying latest security patches just to be on the safe side

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -581,6 +581,7 @@ def create_and_enroll_user(email, username, name, country, password, course_id, 
 
 
 @require_POST
+@require_POST
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 @require_level('staff')
@@ -911,7 +912,6 @@ def modify_access(request, course_id):
         'success': 'yes',
     }
     return JsonResponse(response_payload)
-
 
 @require_POST
 @ensure_csrf_cookie
@@ -2232,6 +2232,7 @@ def rescore_entrance_exam(request, course_id):
 
 
 @require_POST
+@require_POST
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 @require_level('staff')
@@ -2360,6 +2361,7 @@ def list_entrance_exam_instructor_tasks(request, course_id):  # pylint: disable=
     return JsonResponse(response_payload)
 
 
+@require_POST
 @require_POST
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
@@ -2877,6 +2879,7 @@ def mark_student_can_skip_entrance_exam(request, course_id):  # pylint: disable=
 
 
 @transaction.non_atomic_requests
+@require_POST
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 @require_global_staff
@@ -2899,6 +2902,7 @@ def start_certificate_generation(request, course_id):
 
 
 @transaction.non_atomic_requests
+@require_POST
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 @require_global_staff
@@ -2941,6 +2945,7 @@ def start_certificate_regeneration(request, course_id):
 
 
 @transaction.non_atomic_requests
+@require_POST
 @ensure_csrf_cookie
 @cache_control(no_cache=True, no_store=True, must_revalidate=True)
 @require_global_staff
