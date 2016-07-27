540f29b58f2f1880900407f82b76c9f995aa70a6
Pierre Mailhot <pierre.mailhot@gmail.com>
2016-07-26 20:32:01 -0400

    Update common.py
    put back milestones

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2260,7 +2260,7 @@ INSTALLED_APPS = [
     'sorl.thumbnail',
 
     # edx-milestones service
-    #'milestones',
+    'milestones',
 
     # Gating of course content
     'gating.apps.GatingConfig',
