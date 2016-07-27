6e8850aa88cf919c5919cd93feed0cd342f6ebdc
Pierre Mailhot <pierre.mailhot@gmail.com>
2016-07-26 20:32:01 -0400

    Update common.py
    put back milestones

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2261,7 +2261,7 @@ INSTALLED_APPS = [
     'openedx.core.djangoapps.credentials',
 
     # edx-milestones service
-    #'milestones',
+    'milestones',
 
     # Gating of course content
     'gating.apps.GatingConfig',
