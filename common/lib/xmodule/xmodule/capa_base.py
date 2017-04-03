5ce1d16bea139d928ee60c90d621721e05cac4e1
sambapete <pierre.mailhot@gmail.com>
2017-04-03 19:12:30 -0400

    Original string on two lines did not match content in Transifex because of added white space. Fixed.

--- common/lib/xmodule/xmodule/capa_base.py
+++ common/lib/xmodule/xmodule/capa_base.py
@@ -683,9 +683,7 @@ class CapaMixin(CapaFields):
 
         save_message = None
         if self.has_saved_answers:
-            save_message = _(
-                "Your answers were previously saved. Click '{button_name}' to grade them."
-            ).format(button_name=self.submit_button_name())
+            save_message=_("Your answers were previously saved. Click '{button_name}' to grade them.").format(button_name=self.submit_button_name())
 
         context = {
             'problem': content,
