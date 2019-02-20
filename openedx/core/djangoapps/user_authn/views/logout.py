295320acc2170dbcc8b3b835195c9165f84d1dc3
Felipe Montoya <felipe.montoya@edunext.co>
2019-02-20 16:48:46 -0500

    Make the studio login over the lms optional using a feature flag
    Squashed from:
    
    80b977fff4 Make the studio login over the lms optional using a feature flag
    14b4223b5e Addressing feedback
    9195ec9f30 Addressing second feedback about redirect logic on logout behing feature flag
    923a91734d Fixing lettuce tests
    
    (cherry picked from commit 6f91a0d9e8ea2364f58a2cd38e804ac57f8f416b)
    
    Commit 6f91a0d9 was a squashed version of the four original commits, to
    make resolving conflicts easier.

--- openedx/core/djangoapps/user_authn/views/logout.py
+++ openedx/core/djangoapps/user_authn/views/logout.py
@@ -77,6 +77,13 @@ class LogoutView(TemplateView):
         logout(request)
 
         response = super(LogoutView, self).dispatch(request, *args, **kwargs)
+##### Code Ironwood #####
+#        # If we are using studio logout directly and there is not OIDC logouts we can just redirect the user
+#        if settings.FEATURES.get('DISABLE_STUDIO_SSO_OVER_LMS', False) and not self.oauth_client_ids:
+#            response = redirect(self.target)
+#        else:
+#            response = super(LogoutView, self).dispatch(request, *args, **kwargs)
+##### Code Ironwood #####
 
         # Clear the cookie used by the edx.org marketing site
         delete_logged_in_cookies(response)
